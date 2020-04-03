3bfda89556312f3658439672d44312e8bc103988
Matt <mhopps108@users.noreply.github.com>
2020-04-03 14:12:42 -0700

    Updates

---
+++ precache-manifest.571fec05439f63e2776b4b4b8f71493f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d902f4619b42089ccc02b14faf8ac7",
+    "url": "/testreactapp/index.html"
+  },
+  {
+    "revision": "db23a30a4ce5d970b1f7",
+    "url": "/testreactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e741ebf29fb13f377b1a",
+    "url": "/testreactapp/static/js/2.4a83f165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testreactapp/static/js/2.4a83f165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db23a30a4ce5d970b1f7",
+    "url": "/testreactapp/static/js/main.fc968ed2.chunk.js"
+  },
+  {
+    "revision": "ecbbc137b25676e8c4ca",
+    "url": "/testreactapp/static/js/runtime-main.81b84e60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
